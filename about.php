203f4a719446b4badc8b1bca30eb0c2a2d8855ef
KenedyMwendwa <mulilamwendwa@gmail.com>
2021-11-01 22:48:00 +0400

    about us page

---
+++ about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE
+ html>
+<html lang="en">
+<head>
+<title>about</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, 
+initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+* {
+  box-sizing: border-box;
+}
+body { font-family: Arial, Helvetica, sans-serif; width:;}
+h3{text-align: center;}
+h1{text-align: center;}
+p{text-align:; margin-right: 20px; margin-left: 20px; line-height: 180%;}
+img{border-radius:5px;}
+
+.pageheading{margin:auto;}
+.formheading{color:green; font-family: Brush Script-MT;}
+
+.aboutcontainer{ background: silver; padding: 10px; width: 100%; margin: auto;}
+.aboutwrapper{   width:90%; border: 0px solid #a1a1a1; margin: auto;}
+.aboutform-container{ margin: auto; padding: 15px; width: 95%;background: #F5F5F5; margin-top: -3%; border-radius:10px;
+}
+.aboutform{padding: 15px; 
+	border-radius: 20px;
+	margin-top: 2px; 
+ 	width: 100%; 
+ 	display: inline-block;position: inherit;
+ 		 }
+.aboutcontent{line-height: 180%;}
+.checked {color: green;}
+.star{text-align:center;}
+.foundercolumn{width:33.33%; float:left; text-align:center;}
+.roles{margin-top:-15px; font-style: italic;}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+
+@media screen and (max-width:600px) {
+  body{width: auto; height: auto;}
+  .column{width: 100%; float:left; margin: 5px; padding: 3px;}
+  .aboutwrapper{width: 98%;padding: 3px;}
+}
+
+
+
+</style>
+</head>
+<body>
+<div>
+
+  <!-- wrapper -->
+  <div class="aboutwrapper"> 
+
+  <!-- insert header here -->
+
+                <!-- page heading -->
+                <!-- close page heading  -->
+
+<!-- aboutus, mission and vision row -->
+<div class="aboutcontainer">
+            <div class="row">
+                                <!-- start column- a -->
+                                    <div class="aboutcolumn">
+                                        <div class="aboutform-container">
+                                            <div class="aboutform">
+                                                <div class="row">
+                                                <!-- about us goeshere -->
+                                                        <div class="formheading"><h3>About Us</h3></div>
+
+                                                <div class="row">
+                                                    <div class="aboutcontent">
+                                                            <p>
+                                                            Baotech online solutions works on providing technological unfolding to businesses, organizations and other institutions. We ensure this through designing creative user experience for full satisfaction of the customers and by optimizing the website effectively. Similarly, we build excellent fitting systems for the organization that provide enhanced solutions and also connect them to strong and reliable networks for consistent communication. With the increased competition in the market, we implement online marketing solutions with the aim of enabling businesses attain their goals.</br>
+                                                            Baotech targets the less developed organizations to ensure they are not left behind in the fast-transforming world of technology. We believe that with the right tools and ICT skills, our target will be met and the target group will survive in the evolving technological patterns in communication, marketing, website development and system enhancement. </br>
+                                                            The five reasons as to why you should choose us; to enhance your sales through advanced online marketing solutions, reach more customers through affordable and reliable web hosting services, have access to strong and reliable network, own best fit systems and optimize your websites effectively.   
+                                                            </p>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="star">
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="foundercolumn">
+                                                    <img src="img/hacker.jpg" alt="" width="170" height="150">
+                                                        <p>Kenedy M.</p>
+                                                        <div class="roles">
+                                                            <p>(Founder-Programmer.)</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="foundercolumn">
+                                                    <img src="img/hacker.jpg" alt="" width="170" height="150">
+                                                        <p>Kenedy M.</p>
+                                                        <div class="roles">
+                                                            <p>(Founder-Programmer.)</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="foundercolumn">
+                                                    <img src="img/hacker.jpg" alt="" width="170" height="150">
+                                                        <p>Kenedy M.</p>
+                                                        <div class="roles">
+                                                            <p>(Founder-Programmer.)</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <!-- end columna -->
+
+            </div>
+            <!-- end aboutus, mission and vision row -->
+
+
+</div>
+
+
+<!-- insert footer here -->
+
+
+</div>
+<!-- end wrapper -->
+<!-- services ends here -->
+
+
+<!-- footer -->
+<div>
+
+</div>
+<!-- end footer -->
+</body>
+</html>
